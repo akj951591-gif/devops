2c887305b18dbad442add57865a27af2bf7648c0
akj951591-gif <akj951591@gmail.com>
2026-04-03 21:14:57 +0530

    Changes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-    });
+    res.sendFile(path.resolve('index.html'));
+});
 
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
