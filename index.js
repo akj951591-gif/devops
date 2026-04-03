b540b3723359fe668a941333733f26ffe5b8288f
akj951591-gif <akj951591@gmail.com>
2026-04-03 21:07:28 +0530

    alpha

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
